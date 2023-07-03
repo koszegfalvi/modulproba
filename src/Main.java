adaa4536086a79b263d9aa088c6ded5d3c8b4609
koszegfalvi <128134080+koszegfalvi@users.noreply.github.com>
2023-07-03 20:04:04 +0200

    almost ready

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
+import animal.Animal;
 import animal.Elephant;
+import animal.Foka;
+import animal.Lion;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        Elephant elephant=new Elephant("Jumbo");
+
+    }
+
+    public void makeNoise(Animal animal) {
+        if (animal instanceof Elephant) {
+            System.out.println("huuuuuuuuu");
+
+        }
+        if (animal instanceof Foka) {
+            System.out.println("hahahaha");
+        }
+        if (animal instanceof Lion) {
+            System.out.println("hoooooooooo");
+        }
+
+
+    }
+    public void kiiratasCsaladfa(String motherorfather){
+        if(motherorfather.equals("mother")){
+            System.out.println();
+        }
+        else{
+            System.out.println();
+        }
     }
 
 }
